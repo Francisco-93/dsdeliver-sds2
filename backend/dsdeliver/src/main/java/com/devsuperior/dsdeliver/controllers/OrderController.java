dd643d6d7be1c5db4dad571cb08099b355452883
Francisco-93 <francisco.fro.agui@gmail.com>
2021-01-05 21:49:48 -0300

    PUT /orders/{id}/delivered

--- backend/dsdeliver/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/dsdeliver/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,12 @@ public class OrderController {
 	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
 		dto = service.insert(dto);
 		return new ResponseEntity<OrderDTO>(dto, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		OrderDTO dto = service.setDelivered(id);
+		return new ResponseEntity<OrderDTO>(dto, HttpStatus.OK);
 	}
 
 }
